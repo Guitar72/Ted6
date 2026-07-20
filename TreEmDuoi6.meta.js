e3b2af2f204cbffcff63037e574dfd14c35d24c2
Guitar72 <dhabolero@gmail.com>
2026-07-20 08:01:53 +0700

    Update TreEmDuoi6.meta.js

--- TreEmDuoi6.meta.js
+++ TreEmDuoi6.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TreEmDuoi6
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Nut Thao tac nhanh (menu: Kham lam sang, Cap nhat phien ban, Tac gia)
 // @author       Auto-generated
 // @match        https://quanlyskcd.medinet.org.vn/*
@@ -15,6 +15,7 @@
 // ==/UserScript==
 
 // ==Changelog==
+// 1.3 | 2026-07-19 | Thay doi cap nhat
 // 1.2 | 2026-07-19 | Them muc "Kham lam sang" tu dong chon 48 truong radio trong form Kham lam sang tre duoi 6 tuoi
 // 1.1 | 2026-07-19 | Doi nut ve lai "Thao tac nhanh" dang dropdown menu voi 2 muc: Cap nhat phien ban, Tac gia
 // 1.0 | 2026-07-19 | Phien ban dau tien: nut chen truoc "Luu", he thong license bao mat (machine-id + ma kich hoat)
